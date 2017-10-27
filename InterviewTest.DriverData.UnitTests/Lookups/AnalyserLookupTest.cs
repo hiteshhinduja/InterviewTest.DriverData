968b9ac519f93abcd3ce439484e29aa66a65dd4f
hitesh.hinduja <hitesh.hinduja@RAV-DSK-419>
2017-10-27 11:39:53 +0530

    Task 2 - Better Analyser Lookup

---
+++ InterviewTest.DriverData.UnitTests/Lookups/AnalyserLookupTest.cs
@@ -0,0 +1,67 @@
+﻿using InterviewTest.DriverData.Analysers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTest.DriverData.UnitTests.Lookups
+{
+    [TestFixture]
+    public class AnalyserLookupTest
+    {
+        [Test]
+        public void ShouldCreateDeliveryDriverAnalyserInstance()
+        {
+            //Arrange
+            var analyserType = "Delivery";
+            //Act
+            var analyserInstance = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.IsInstanceOf(typeof(DeliveryDriverAnalyser), analyserInstance);
+        }
+
+        [Test]
+        public void ShouldCreateFormulaOneDriverAnalyserInstance()
+        {
+            //Arrange
+            var analyserType = "FormulaOne";
+            //Act
+            var analyserInstance = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.IsInstanceOf(typeof(FormulaOneAnalyser), analyserInstance);
+        }
+
+        [Test]
+        public void ShouldCreateGetawayDriverAnalyserInstance()
+        {
+            //Arrange
+            var analyserType = "Getaway";
+            //Act
+            var analyserInstance = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.IsInstanceOf(typeof(GetawayDriverAnalyser), analyserInstance);
+        }
+
+        [Test]
+        public void ShouldCreateFriendlyAnalyserInstance()
+        {
+            //Arrange
+            var analyserType = "Friendly";
+            //Act
+            var analyserInstance = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.IsInstanceOf(typeof(FriendlyAnalyser), analyserInstance);
+        }
+
+        [Test]
+        public void ShouldThrowArgumentOutOfRangeException()
+        {
+            //Arrange
+            var analyserType = "SomeOtherDriver";
+            //Act & Assert
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => AnalyserLookup.GetAnalyser(analyserType));
+        }
+    }
+}
--- InterviewTest.DriverData/AnalyserLookup.cs
+++ InterviewTest.DriverData/AnalyserLookup.cs
@@ -1,27 +1,26 @@
 using System;
 using InterviewTest.DriverData.Analysers;
 using InterviewTest.DriverData.Entities;
+using System.Collections.Generic;
 
 namespace InterviewTest.DriverData
 {
 	public static class AnalyserLookup
 	{
-		public static IAnalyser GetAnalyser(string type)
-		{
-			switch (type)
-			{
-                case "delivery":
-                    return new DeliveryDriverAnalyser(new AnalyserConfiguration() { StartTime = new TimeSpan(9, 0, 0), EndTime = new TimeSpan(17, 0, 0), MaxSpeed = 30m, RatingForExceedingMaxSpeed = 0, PenaltyForFaultyRecording = 0.5m });
-                case "formulaOne":
-                    return new FormulaOneAnalyser(new AnalyserConfiguration() { MaxSpeed = 200m, RatingForExceedingMaxSpeed = 1, PenaltyForFaultyRecording = 0.5m });
-                case "getaway":
-                    return new GetawayDriverAnalyser(new AnalyserConfiguration() { StartTime = new TimeSpan(13, 0, 0), EndTime = new TimeSpan(14, 0, 0), MaxSpeed = 80m, RatingForExceedingMaxSpeed = 1, PenaltyForFaultyRecording = 0.5m });
-				case "friendly":
-					return new FriendlyAnalyser();
-
-				default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
-			}
-		}
-	}
+        private static Dictionary<string, Func<IAnalyser>> analysers = new Dictionary<string, Func<IAnalyser>>()
+        {
+            {"Delivery", () => {return new DeliveryDriverAnalyser(new AnalyserConfiguration() { StartTime = new TimeSpan(9, 0, 0), EndTime = new TimeSpan(17, 0, 0), MaxSpeed = 30m, RatingForExceedingMaxSpeed = 0, PenaltyForFaultyRecording = 0.5m }); } },
+            {"FormulaOne", () => {return new FormulaOneAnalyser(new AnalyserConfiguration() { MaxSpeed = 200m, RatingForExceedingMaxSpeed = 1, PenaltyForFaultyRecording = 0.5m }); } },
+            {"Getaway", () => {return new GetawayDriverAnalyser(new AnalyserConfiguration() { StartTime = new TimeSpan(13, 0, 0), EndTime = new TimeSpan(14, 0, 0), MaxSpeed = 80m, RatingForExceedingMaxSpeed = 1, PenaltyForFaultyRecording = 0.5m }); } },
+            {"Friendly", () => {return new FriendlyAnalyser(); } }
+        };
+        public static IAnalyser GetAnalyser(string type)
+        {
+            if (analysers.ContainsKey(type))
+            {
+                return analysers[type]();
+            }
+            throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
+        }
+    }
 }
